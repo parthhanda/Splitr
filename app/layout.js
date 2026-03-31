ee9b80331f9e18681ce66335dd7385dc59e1e15f
Parth Handa <phcodepro@gmail.com>
2026-03-31 17:31:28 +0530

    Added Groups page

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/header";
 import { Inter } from "next/font/google";
-import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "@/components/ConvexClientProvider";
+import Header from "@/components/header";
+import { Toaster } from "sonner";
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Splitr",
@@ -17,19 +14,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-    >
+    <html lang="en">
       <head>
         <link rel="icon" href="/logos/logo-s.png" sizes="any" />
       </head>
       <body className={`${inter.className}`}>
-        <ClerkProvider>
-        <ConvexClientProvider>
-            
-        <Header/>
-        <main className="min-h-screen">{children}</main>
-        </ConvexClientProvider>
+        <ClerkProvider
+          publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+        >
+          <ConvexClientProvider>
+            <Header />
+            <main className="min-h-screen">
+              <Toaster richColors />
+
+              {children}
+            </main>
+          </ConvexClientProvider>
         </ClerkProvider>
       </body>
     </html>
